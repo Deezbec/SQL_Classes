5acc8f9dbeadc6b4d4a747b1f20b0149eeee7e9d
Deezbec <greger.ert@gmail.com>
2020-03-21 15:07:59 +0300

    SQL lesson 20.03.20

---
+++ SQL/SQL/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists(@"C:\User\!Kirill\SQL_Classes\Test.db"))
+            {
+                SQLiteConnection.CreateFile(@"C:\User\!Kirill\SQL_Classes\Test.db");
+                Console.WriteLine("Создана БД");
+            }
+            else Console.WriteLine("БД уже создана");
+            SQLiteConnection connect = new SQLiteConnection(@"Data Source=C:\User\!Kirill\SQL_Classes\Test.db; Version=3;");
+            connect.Open();
+
+            SQLiteCommand comandSQL;
+            comandSQL = new SQLiteCommand("CREATE TABLE IF NOT EXISTS \"Employees\"" + "(\"id\" INTEGER PRIMARY KEY AUTOINCREMENT, \"name\" TEXT, \"section\" TEXT);", connect);
+            comandSQL.ExecuteNonQuery();
+            comandSQL = new SQLiteCommand("INSERT INTO \"Employees\" (\"name\", \"section\") " + "VALUES (\"KIRILL\", \"RemoteControl\")", connect);
+            comandSQL.ExecuteNonQuery();
+            comandSQL = new SQLiteCommand("SELECT * FROM \"Employees\"", connect);
+            SQLiteDataReader reader = comandSQL.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("ID " + reader["id"] + " NAME " + reader["name"]
+                    + " INFO " + reader["section"]);
+            }
+            reader.Close();
+            connect.Close(); 
+            Console.ReadLine();
+        }
+    }
+}
